6d9fb17e1df2e86dfff2d4c50c64577cc86c0797
Louie Hansen <gamernxt6@gmail.com>
2024-01-06 20:38:09 +0700

    init preview

---
+++ src/pages/preview.tsx
@@ -0,0 +1,31 @@
+import { ClientEvents } from "@LyricSync/types"
+import { useState, useEffect } from "react"
+import { Socket, io } from "socket.io-client"
+
+export default function Preview() {
+  const [Socket, setSocket] = useState<Socket<ClientEvents> | null>()
+
+  useEffect(() => {
+    SocketInitializer()
+  }, [])
+
+  const SocketInitializer = () => {
+    fetch('/api/socket')
+    const socket = io()
+
+    socket.on('connect', () => {
+      console.log('connected', socket.id)
+      setSocket(socket)
+
+      socket.emit('RegisterClient', 'Preview')
+    })
+  }
+
+  return (
+    <div className="flex text-center items-center justify-center min-h-full">
+      <h1 className="text-center m-auto font-bold text-wrap lg:text-9xl">
+        <LyricPreview />
+      </h1>
+    </div>
+  )
+}
