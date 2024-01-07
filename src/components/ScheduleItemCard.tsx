7487ff4eaaead3383464d405c3e55a8bb57a1a04
Louie Hansen <gamernxt6@gmail.com>
2024-01-07 21:13:53 +0700

    fixed schedule numbering

--- src/components/ScheduleItemCard.tsx
+++ src/components/ScheduleItemCard.tsx
@@ -14,7 +14,7 @@ export default function ScheduleItemCard({
   return (
     <div className="cursor-pointer inline">
       <div data-rbdndraghandle>
-        <span className="m-1 border-r-2 p-1">{item.index}</span>
+        <span className="m-1 border-r-2 p-1">{item.index + 1}</span>
         <span>{item.title}</span>
       </div>
     </div>
