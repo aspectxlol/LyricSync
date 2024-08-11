d0c3476b386fdafdf672c884ff81972140b654d7
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-08-11 12:44:57 +0700

    express + socket.io

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,24 @@
 import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import 'dotenv/config'
 
 const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, { cors: { origin: ['*'] } })
 
-app.get('/', () => { })
-
-app.listen('3000', () => {
-  console.log('Listening on Port 3000')
+io.on('connect', (socket) => {
+  console.log(socket.id)
 })
+
+httpServer.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on Port ${process.env.PORT}`)
+})
+
+// const app = express()
+
+// app.get('/', () => { })
+
+// app.listen('3000', () => {
+//   console.log('Listening on Port 3000')
+// })
