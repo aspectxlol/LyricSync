6de7bb548ff3a96a1af88a1a2e447f5701a8668c
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-08-12 19:10:14 +0700

    realize i dont need postgres

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,12 +1,8 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/bun-sqlite';
 import * as schema from "./schema";
+import { Database } from 'bun:sqlite'
 
-const migrationClient = postgres(process.env.DATABASE_URL!, { max: 1 });
-migrate(drizzle(migrationClient), { migrationsFolder: "/db/migrations" })
-
-const queryClient = postgres(process.env.DATABASE_URL!);
-const db = drizzle(queryClient, { schema });
+const sqlite = new Database(process.env.DATABASE_URL!)
+const db = drizzle(sqlite, { schema });
 
 export { db }
