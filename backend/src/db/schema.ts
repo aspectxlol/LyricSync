0331e8d82b25ab90c13269ff0e168b74fca2c0e0
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-08-12 15:26:21 +0700

    remove createTAble thing

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,24 +1,22 @@
 import {
   bigint,
   char,
-  pgTableCreator
+	pgTable,
 } from "drizzle-orm/pg-core";
 
-export const createTable = pgTableCreator((name) => `lyricsync_${name}`);
-
-export const song = createTable("Song", {
+export const song = pgTable("Song", {
 	id: bigint("id", { mode: "number" }).primaryKey().notNull(),
 	title: char("Title", { length: 255 }).notNull(),
 	author: char("Author", { length: 255 }).notNull(),
 	lyric: bigint("Lyric", { mode: "number" }).notNull().references(() => lyric.id),
 });
 
-export const schedule = createTable("Schedule", {
+export const schedule = pgTable("Schedule", {
 	id: bigint("id", { mode: "number" }).primaryKey().notNull(),
 	songs: bigint("Songs", { mode: "number" }).notNull().references(() => song.id),
 });
 
-export const lyric = createTable("Lyric", {
+export const lyric = pgTable("Lyric", {
 	id: bigint("id", { mode: "number" }).primaryKey().notNull(),
 	content: char("content", { length: 255 }).notNull(),
 });
